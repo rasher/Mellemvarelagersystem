ca853ef22dba0013d8f7ea96eac7e2573e748981
pederdingo <warquesniff@hotmail.com>
2010-11-15 10:48:33 +0100

    Service createMellemvare

--- model/Service.java
+++ model/Service.java
@@ -1,10 +1,13 @@
 package model;
 
 import java.util.List;
+import javax.persistence.*;
 
 public class Service {
 	public Mellemvare createMellemvare(Produkttype produkttype)
 	{
+		Mellemvare nyMellemvare = new Mellemvare();
+		nyMellemvare.setProdukttype(produkttype);
 		return null;
 	}
 	public Produkttype createProdukttype(Behandling behandling)
