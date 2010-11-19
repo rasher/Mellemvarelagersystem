768bc6e3432af2539892674f43a71a3b1d749e9a
Jonas Häggqvist <rasher@gmail.com>
2010-11-19 11:55:52 +0100

    fix nogle warnings ved at ordne imports, tilføje serials og ignorere en ubrugt attribut

--- gui/OpretBehandlingPanel.java
+++ gui/OpretBehandlingPanel.java
@@ -4,7 +4,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListModel;
