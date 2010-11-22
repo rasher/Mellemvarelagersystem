236eeda83c5854e5848995a49ca5831a7a93d4a1
Jonas Häggqvist <rasher@gmail.com>
2010-11-22 09:41:33 +0100

    Flyt kode til en fælles setUp metode og tilføj flere testcases

--- jUnitTests/JUnitTestMellemvare.java
+++ jUnitTests/JUnitTestMellemvare.java
@@ -1,6 +1,8 @@
 package jUnitTests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -11,19 +13,17 @@ import model.Delbehandling;
 import model.Mellemvare;
 import model.Produkttype;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class JUnitTestMellemvare {
-	/**
-	 * Test method for {@link model.Mellemvare#beregnTørringsTider()}.
-	 */
-	@Test
-	public void testberegnMinimumTørringsTidTC1() {
-		Delbehandling d;
-		Behandling b;
-		Produkttype p;
-		Mellemvare m;
-		
+	Delbehandling d;
+	Behandling b;
+	Produkttype p;
+	Mellemvare m;
+	
+	@Before
+	public void setUp() {
 		d = new Delbehandling();
 		d.setNavn("Test");
 		d.setMinimumTørringsTid(1);
@@ -45,11 +45,15 @@ public class JUnitTestMellemvare {
 			bht.setDelbehandling(delbehandling);
 			m.addBehandlingsTrin(bht);
 		}		
-		try {
-			m.startDelbehandling();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	}
+	
+	/**
+	 * Test method for {@link model.Mellemvare#beregnTørringsTider()}.
+	 * @throws Exception 
+	 */
+	@Test
+	public void testberegnMinimumTørringsTidTC1() throws Exception {
+		m.startDelbehandling();
 		m.startTørring();
 		
 		GregorianCalendar forventet = new GregorianCalendar();
@@ -68,5 +72,57 @@ public class JUnitTestMellemvare {
 		assertEquals(forventet.getTimeInMillis(), minimumOpnået.getTimeInMillis());
 		assertEquals(forventet.getTimeInMillis(), optimalOpnået.getTimeInMillis(), 2.0);
 		assertEquals(forventet.getTimeInMillis(), maksimumOpnået.getTimeInMillis(), .0);
+	}
+	
+	/**
+	 * Test at der kastes en exception hvis der køres startDelbehandling uden
+	 * at der er oprettet delbehandlinger
+	 * @throws Exception
+	 */
+	@Test(expected = Exception.class)
+	public void testStartDelbehandlingTC2() throws Exception {
+		Behandling b;
+		Produkttype p;
+		Mellemvare m;
+				
+		b = new Behandling();
+		b.setNavn("Test");
+		
+		p = new Produkttype();
+		p.setNavn("Test");
+		p.setBehandling(b);
+
+		m = new Mellemvare();
+		m.setProdukttype(p);
+		for (Delbehandling delbehandling: p.getDelbehandlinger()) {
+			BehandlingsTrin bht = new BehandlingsTrin();
+			bht.setDelbehandling(delbehandling);
+			m.addBehandlingsTrin(bht);
+		}		
+		m.startDelbehandling();
+	}
+
+	/**
+	 * Test at der kastes en exception hvis der køres startDelbehandling uden
+     * at der findes en delbehandling der ikke er begyndt
+	 * @throws Exception
+	 */
+	@Test(expected = Exception.class)
+	public void testStartDelbehandlingTC3() throws Exception {
+		m.startDelbehandling();
+		// Den egentlige test:
+		m.startDelbehandling();
+	}
+
+	/**
+	 * Test at der der returneres et behandlingstrin efter at en delbehandling
+	 * er igangsat
+	 * @throws Exception
+	 */
+	@Test
+	public void testStartDelbehandlingTC4() throws Exception {
+		m.startDelbehandling();
+		assertNotNull(m.getAktueltBehandlingTrin());
+		assertSame(m.getAktueltBehandlingTrin().getDelbehandling(), d);
 	}
 }
