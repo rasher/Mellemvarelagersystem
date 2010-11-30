ba57dbf9ebef14b23fe2bea0678e889affd27d2d
Jonas Häggqvist <rasher@gmail.com>
2010-11-30 12:01:53 +0100

    Tilføj javadoc

--- model/Behandling.java
+++ model/Behandling.java
@@ -3,7 +3,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.*;
-
+/**
+ * En Behandling
+ * 
+ * @author Peter Runge Christensen
+ *
+ */
 @Entity
 @Table(name="Behandling")
 @NamedQuery(name="findAlleBehandlinger", query="Select b from Behandling b")
@@ -17,29 +22,59 @@ public class Behandling {
 	@OrderBy(value="rækkefølge ASC")
 	private List<BehandlingDelbehandlingRelation> behandlingDelbehandlingRelationer = 
 		new ArrayList<BehandlingDelbehandlingRelation>();
+	
+	
+	/**
+	 * Returner Behandlingens navn
+	 * @return Behandlingens navn
+	 */
 	public String getNavn() {
 		return navn;
 	}
+	
+	/**
+	 * Sæt Behandlingens navn
+	 * @param navn Det nye navn
+	 */
 	public void setNavn(String navn) {
 		this.navn = navn;
 	}
 	
-	
+	/**
+	 * tilføj en BehandlingDelbehandlingRelation
+	 * @param bdr den nye BehandlingDelbehandlingRelation
+	 */
 	public void addBehandlingDelbehandlingRelation(BehandlingDelbehandlingRelation bdr)
 	{
 		this.behandlingDelbehandlingRelationer.add(bdr);
 	}
+	
+	/**
+	 * Returner listen af alle BehandlingDelbehandlingRelationer
+	 * @return Listen af alle BehandlingDelbehandlingRelationer
+	 */
 	public List<BehandlingDelbehandlingRelation> getBehandlingDelbehandlingRelationer() {
 		return behandlingDelbehandlingRelationer;
 	}
+	
+	/**
+	 * Sæt listen af alle BehandlingDelbehandlingRelation
+	 * @param behandlingDelbehandlingRelation den nye liste af BehandlingDelbehandlingRelationer
+	 */
 	public void setBehandlingDelbehandlingRelationer(
 			List<BehandlingDelbehandlingRelation> behandlingDelbehandlingRelation) {
 		this.behandlingDelbehandlingRelationer = behandlingDelbehandlingRelation;
 	}
+
+	/**
+	 * Fjern en BehandlingDelbehandlingRelation
+	 * @param bdr BehandlingDelbehandlingRelationen der skal fjernes
+	 */
 	public void removeBehandlingDelbehandlingRelation(BehandlingDelbehandlingRelation bdr)
 	{
 		this.behandlingDelbehandlingRelationer.remove(bdr);
 	}
+	
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
