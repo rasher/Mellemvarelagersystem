227d17e804733414814b7371ed93f3396740ef6a
Jonas Häggqvist <rasher@gmail.com>
2010-12-06 13:13:00 +0100

    Funktion til at indtaste produkttype

--- caos/OpretMellemvare.java
+++ caos/OpretMellemvare.java
@@ -1,6 +1,9 @@
 package caos;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 /**
@@ -21,5 +24,25 @@ public class OpretMellemvare {
 		System.out.println("");
 		scanner = new Scanner(System.in);
 		conn = Database.getConnection();
+		int produktType = vælgProdukttype();
+	}
+
+	/**
+	 * @return Det indtastede id
+	 */
+	private static int vælgProdukttype() {
+		System.out.println("Produkttyper:");
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet res = stmt.executeQuery("SELECT ID, NAVN FROM Produkttype ORDER BY NAVN");
+			while (res.next()) {
+				String out = String.format("  %2d. %s", res.getInt("ID"), res.getString("NAVN"));
+				System.out.println(out);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
 	}
 }
