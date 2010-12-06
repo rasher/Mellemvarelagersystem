5e692262816d06bedfd7145109b1c01c269219f6
Jonas Häggqvist <rasher@gmail.com>
2010-12-06 12:56:20 +0100

    Opret caos package med Database singleton klasse

---
+++ caos/Database.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package caos;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+/**
+ * @author Jonas Häggqvist (10dm10v1)
+ *
+ */
+public class Database {
+	private static Connection connection;
+	
+	/**
+	 * Få en instans af Service klassen
+	 * @return Service klasse objekt
+	 */
+	public static Connection getConnection()
+	{
+		String filnavn = "jdbcsettings.txt";
+		if(connection == null) {
+			Scanner scanner;
+			try {
+				scanner = new Scanner(new File(filnavn));
+				connection = DriverManager.getConnection(scanner.nextLine());
+			} catch (FileNotFoundException e) {
+				System.err.println("Indsæt din jdbc url i filen " + filnavn);
+			} catch (SQLException e) {
+				System.err.println("Kunne ikke forbinde til databasen");
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
+	
+	public static void closeConnection()
+	{
+		if(connection != null)
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				System.err.println("Kunne ikke lukke databaseforbindelse");
+				e.printStackTrace();
+			}
+	}
+	
+	/**
+	 * Constructor
+	 */
+	private Database()
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+		Connection conn = getConnection();
+		closeConnection();
+	}
+}
