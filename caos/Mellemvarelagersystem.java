43fe22bfb40535e2e1df5b9663d8827d6328fdb0
Jonas Häggqvist <rasher@gmail.com>
2010-12-07 11:32:43 +0100

    En hovedmenu (der ikke gør noget endnu)

---
+++ caos/Mellemvarelagersystem.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package caos;
+
+/**
+ * @author Jonas Häggqvist (10dm10v1)
+ *
+ */
+public class Mellemvarelagersystem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("**********************************");
+		System.out.println("* Carletti mellemvarelagersystem *");
+		System.out.println("**********************************\n");
+		System.out.println("Vælg ønsket handling:");
+		System.out.println(" 1. Registrer ny mellemvare");
+		System.out.println(" 2. Vis mellemvarer");
+		System.out.println(" 3. Beregn tid på lager for mellemvare");
+	}
+
+}
