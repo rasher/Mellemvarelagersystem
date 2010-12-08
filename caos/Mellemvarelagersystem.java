3cbfd00be3c5f0f595950a22a47a351ccc3cf7e2
Jonas Häggqvist <rasher@gmail.com>
2010-12-08 09:24:08 +0100

    Lav noget der ligner en menu der måske endda virker

--- caos/Mellemvarelagersystem.java
+++ caos/Mellemvarelagersystem.java
@@ -3,6 +3,8 @@
  */
 package caos;
 
+import java.util.Scanner;
+
 /**
  * @author Jonas Häggqvist (10dm10v1)
  *
@@ -16,10 +18,40 @@ public class Mellemvarelagersystem {
 		System.out.println("**********************************");
 		System.out.println("* Carletti mellemvarelagersystem *");
 		System.out.println("**********************************\n");
-		System.out.println("Vælg ønsket handling:");
-		System.out.println(" 1. Registrer ny mellemvare");
-		System.out.println(" 2. Vis mellemvarer");
-		System.out.println(" 3. Beregn tid på lager for mellemvare");
+		
+		int valg = -1;
+		Scanner scanner = new Scanner(System.in);
+		while (valg != 99) {
+			System.out.println("Hovedmenu. Vælg ønsket handling:");
+			System.out.println(" 1. Udtag vare til delbehandling");
+			System.out.println(" 2. Vis alle mellemvarer");
+			System.out.println(" 3. Beregn tid på lager for mellemvare");
+			System.out.println(" 4. Vis mellemvarer fra produkttype");
+			System.out.println(" 5. Registrer ny mellemvare");
+			System.out.println(" 6. Ryd op i gamle data");
+			System.out.println("99. Afslut");
+			valg = scanner.nextInt();
+			switch (valg) {
+			case 5:
+				OpretMellemvare.registrerNyMellemvare();
+				break;
+			case 4:
+				MellemvareFraProdukttype.main(new String[]{});
+				break;
+			case 3:
+				break;
+			case 2:
+				visMellemvarer.main(new String[]{});
+				break;
+			case 1:
+				UdtagTilDelbehandling.udtagTilDelbehandling();
+				break;
+			case 99:
+				break;
+			default:
+				System.err.println("Ugyldigt valg.");
+				break;
+			}
+		}
 	}
-
 }
