d6be8d3e413deda6ac7c688cb9ef1fd372a25bd0
Jonas Häggqvist <rasher@gmail.com>
2010-12-08 12:40:52 +0100

    Ryd op i warnings

--- caos/visMaksimaleLagertiderForMellemvarer.java
+++ caos/visMaksimaleLagertiderForMellemvarer.java
@@ -4,11 +4,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
 
 public class visMaksimaleLagertiderForMellemvarer {
 	private static Connection conn;
-	private static Scanner scanner;
 	
 	/**
 	 * @param args
