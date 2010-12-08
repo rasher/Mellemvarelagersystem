e6d040f67b6e466a78b2d1d14fc15bf0f8f2a613
Jonas Häggqvist <rasher@gmail.com>
2010-12-08 12:47:56 +0100

    Ret i menuen så den virker rimeligt med alle funktioner

--- caos/Mellemvarelagersystem.java
+++ caos/Mellemvarelagersystem.java
@@ -22,32 +22,42 @@ public class Mellemvarelagersystem {
 		int valg = -1;
 		Scanner scanner = new Scanner(System.in);
 		while (valg != 99) {
-			System.out.println("\nHovedmenu. Vælg ønsket handling:");
-			System.out.println(" 1. Udtag vare til delbehandling");
-			System.out.println(" 2. Vis alle mellemvarer");
-			System.out.println(" 3. Beregn tid på lager for mellemvare");
-			System.out.println(" 4. Vis mellemvarer fra produkttype");
-			System.out.println(" 5. Registrer ny mellemvare");
-			System.out.println(" 6. Ryd op i gamle data");
-			System.out.println("99. Afslut");
+			System.out.println("\nHovedmenu.");
+			System.out.println("  1. Udtag vare til delbehandling");
+			System.out.println("  2. Vis alle mellemvarer");
+			System.out.println("  3. Beregn tid på lager for mellemvare");
+			System.out.println("  4. Vis mellemvarer fra produkttype");
+			System.out.println("  5. Registrer ny mellemvare");
+			System.out.println("  6. Ryd op i gamle data");
+			System.out.println(" 80. Test samtidig tilgang af udtag til delbehandling");
+			System.out.println(" 81. Opret stored procedure til oprydning af gamle data");
+			System.out.println(" 99. Afslut");
+			System.out.print("\nVælg ønsket handling: ");
 			valg = scanner.nextInt();
 			switch (valg) {
-			case 6:
-				System.err.println("Ikke implementeret endnu.");
-			case 5:
-				OpretMellemvare.registrerNyMellemvare();
-				break;
-			case 4:
-				MellemvareFraProdukttype.main(new String[]{});
-				break;
-			case 3:
-				visMaksimaleLagertiderForMellemvarer.main(new String[]{});
+			case 1:
+				UdtagTilDelbehandling.main(new String[]{});
 				break;
 			case 2:
 				visMellemvarer.main(new String[]{});
 				break;
-			case 1:
-				UdtagTilDelbehandling.main(new String[]{});
+			case 3:
+				visMaksimaleLagertiderForMellemvarer.main(new String[]{});
+				break;
+			case 4:
+				MellemvareFraProdukttype.main(new String[]{});
+				break;
+			case 5:
+				OpretMellemvare.registrerNyMellemvare();
+				break;
+			case 6:
+				ExecuteStoredProcedure.main(new String[]{});
+				break;
+			case 80:
+				TestUdtagTilDelbehandling.main(new String[]{});
+				break;
+			case 81:
+				OpretStoredProcedure.main(new String[]{});
 				break;
 			case 99:
 				break;
