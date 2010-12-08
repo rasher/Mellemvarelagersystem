8c13aaa7d00b1fe68d3fc6f20075e563fc9f0cc7
Jonas Häggqvist <rasher@gmail.com>
2010-12-08 12:47:38 +0100

    Ryd op i warnings

--- caos/MellemvareFraProdukttype.java
+++ caos/MellemvareFraProdukttype.java
@@ -3,7 +3,6 @@ package caos;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
