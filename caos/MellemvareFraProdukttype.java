a528b98c393245482a4a223c10fa8d41b30afbb0
pederdingo <warquesniff@hotmail.com>
2010-12-09 09:05:03 +0100

    Rettelser til mellemvare fra produkttype

--- caos/MellemvareFraProdukttype.java
+++ caos/MellemvareFraProdukttype.java
@@ -15,11 +15,15 @@ public class MellemvareFraProdukttype
 			Connection minConnection;
 			minConnection = Database.getConnection();
 			Statement stmt = minConnection.createStatement();
-			System.out.println("Indtast produkttype: ");
+			ResultSet res=stmt.executeQuery("Select produkttype.NAVN from Produkttype");
+			while (res.next()) {
+				System.out.println("Produkttype: " + res.getString(1));
+			}
+			System.out.println("Indtast produkttype navn: ");
 			BufferedReader lineIn = new BufferedReader(new InputStreamReader(System.in));
 			String produkttype= lineIn.readLine();
 			System.out.println("Tilknyttet mellemvarer: ");
-			ResultSet res=stmt.executeQuery("SELECT m.BATCHNUMMER, d.NAVN, bt.RÆKKEFØLGE, (SELECT COUNT(*) FROM " +
+			res=stmt.executeQuery("SELECT m.BATCHNUMMER, d.NAVN, bt.RÆKKEFØLGE, (SELECT COUNT(*) FROM " +
      "Mellemvare_BehandlingsTrin mbt, BehandlingsTrin bt WHERE mbt.Mellemvare_BATCHNUMMER=m.BATCHNUMMER " +
      "AND mbt.behandlingsTrin_ID=bt.ID) as ANTALTRIN FROM Produkttype p RIGHT JOIN Mellemvare m ON p.ID = m.PRODUKTTYPE_ID " +
      "RIGHT JOIN BehandlingsTrin bt ON bt.ID = m.AKTUELBEHANDLINGSTRIN_ID RIGHT JOIN Delbehandling d ON d.ID = bt.DELBEHANDLING_ID " +
