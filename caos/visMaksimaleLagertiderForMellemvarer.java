c7351efe0644b86f62f9eb55e2442dcbfec69b57
Johnny S. Sørensen <Johnny S. Srensen@10.77.33.100>
2010-12-09 08:59:32 +0100

    Vi skal bruge Scanner

--- caos/visMaksimaleLagertiderForMellemvarer.java
+++ caos/visMaksimaleLagertiderForMellemvarer.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class visMaksimaleLagertiderForMellemvarer {
 	private static Connection conn;
